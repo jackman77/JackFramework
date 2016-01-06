ca1bd0381b88b32d506a429a4d516e6034cc8b75
Jimmy Wijaya <6112007@student.unpar.ac.id>
2016-01-06 20:56:41 +0700

    Requests simpel selesai

---
+++ app/System/Request.php
@@ -0,0 +1,72 @@
+<?php
+namespace Jack\System;
+
+class Request
+{
+
+    private $query;
+    public $var = [];
+
+    public function __construct()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $this->query = $_SERVER['QUERY_STRING'];
+
+            $query = explode('&', $this->query);
+            foreach ($query as $qr) {
+                $qr = $this->cleanxss($qr);
+                $expl = explode('=', $qr);
+
+                if (count($expl) == 2) {
+                    $this->$expl[0] = $expl[1];
+                }
+
+            }
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            foreach ($_POST as $post => $value) {
+                $value = $this->cleanxss($value);
+                $this->$post = $value;
+            }
+
+            foreach ($_FILES as $post => $value) {
+                $this->$post = $value;
+            }
+        }
+
+
+    }
+
+    public function __set($name, $value)
+    {
+        $this->var[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->var[$name];
+    }
+
+    public static function cleanxss($data)
+    {
+        $data = str_replace(array('&amp;', '&lt;', '&gt;'), array('&amp;amp;', '&amp;lt;', '&amp;gt;'), $data);
+        $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+        $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+        $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+        $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+        $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+        $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+
+        return $data;
+    }
+
+}
